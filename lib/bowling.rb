aa73f44b02802b11f3be7394a3886710042376cf
miyuki <miyuki@dizi.jp>
2017-01-18 05:51:33 +0000

    スペアを出した場合の計算

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,13 +1,32 @@
 class Bowling
   def initialize
-    @score = 0
+    @total_score = 0
+    
+    @scores = []
+    
+    @frame_pins = []
   end
   
   def add_score(pins)
-    @score += pins 
+   @frame_pins << pins
+   
+    if @frame_pins.size == 2
+      @scores << @frame_pins
+      @frame_pins = []
+    end
   end
   
   def total_score
-    @score
+    @total_score
+  end
+  
+  def calc_score
+    @scores.each.with_index do |score, i|
+      if score.inject(:+) == 10 && i < 9
+        @total_score += 10 + @scores[i + 1].first
+      else
+        @total_score += score.inject(:+)
+      end
+    end
   end
 end
